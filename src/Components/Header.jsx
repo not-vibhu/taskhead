bad2a93ad710bf0d4db4695ce6d77a080636c290
nakulsharma15 <nakuls309@gmail.com>
2022-11-04 23:47:17 +0530

    style: styled header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,20 @@
-import "./Styles/Header.css"
+import "./Styles/Header.css";
 
 export default function Header() {
 
-    return <div className="header">
-        <h1 className="header-main-text">TaskHead</h1>
+    return <div className="header flex-space-between">
+
+        <div className="header-logo flex-align-center">
+
+            <div className="header-logo-img">
+                <img src="https://res.cloudinary.com/nakulsharma15/image/upload/v1667582669/Assets/Screenshot__350_-removebg-preview-removebg-preview_1_1_dxnrge.png" alt="th-logo" />
+            </div>
+
+            <h1>TaskHead</h1>
+        </div>
+
+        <button className="header-button">GitHub</button>
+
     </div>
 
 }
