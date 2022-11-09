33358f988d06db858f557241fc3cfc6ea63639a6
nakulsharma15 <nakuls309@gmail.com>
2022-11-09 23:41:23 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import HomePage from './Pages/HomePage';
+import RoutePaths from './Routes/RoutePaths';
 
 function App() {
   return (
     <div className="App">
-     <HomePage />
+     <RoutePaths />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 import { ModalProvider } from './Contexts/ModalContext';
 import { TaskProvider } from './Contexts/TaskContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TaskProvider>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </TaskProvider>
+    <Router>
+      <TaskProvider>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </TaskProvider>
+    </Router>
   </React.StrictMode>
 );
 
