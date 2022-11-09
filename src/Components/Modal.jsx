e4a983870d48697837ba5d54f12dafc1815f6532
nakulsharma15 <nakuls309@gmail.com>
2022-11-09 14:45:38 +0530

    style: styled modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,39 +1,63 @@
 import "./Styles/Modal.css";
 
-const Modal = () => {
+export default function Modal() {
 
     return (
-        <>
-            <div className="modal-bg">
-                <div className="modal-centered">
+        <div className="modal-bg">
 
-                    <div className="modal">
+            <div className="modal-centered">
 
-                        <div className="modal-header">
-                            <p>Save to...</p>
-                        </div>
+                <div className="modal">
 
-                        <div className="playlist-container">
-
-                            <p>Title goes here</p>
-                            <p>Title goes here</p>
-
-                        </div>
-
-
-
-                        <div className="modal-actions">
-
-                            <div className="actions-container">
-
-                                <button>Action</button>
-                            </div>
-                        </div>
+                    <div className="modal-header">
+                        <p>Add a Task</p>
                     </div>
+
+                    <div className="task-input-container">
+
+                        <p className="task-input-title">Title:</p>
+
+                        <input type="text" placeholder="Enter title" />
+
+                    </div>
+
+                    <div className="priority-input-container">
+
+                        <p className="task-input-title">Priority:</p>
+
+                        <select>
+                            <option value="Low"> Low
+                            </option>
+                            <option value="Medium"> Medium
+                            </option>
+                            <option value="High"> High
+                            </option>
+                        </select>
+
+                    </div>
+
+                    <div className="task-input-container">
+
+                        <p className="task-input-title">Description:</p>
+
+                        <textarea className="task-desc" name="task-description" id="" cols="5" rows="3" placeholder="Enter Description"></textarea>
+
+                    </div>
+
+
+
+                    <div className="modal-actions">
+
+                        <button className="add-button">Add Task</button>
+
+                        <button className="cancel-button">Cancel</button>
+
+                    </div>
+
                 </div>
+
             </div>
-        </>
+
+        </div>
     );
 };
-
-export default Modal;
