b2aa4b9c939d9336d0e6b22cdb9153f43fec8e0a
nakulsharma15 <nakuls309@gmail.com>
2022-11-14 16:47:45 +0530

    style: added bottom border to pomodoro

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import "./Styles/Header.css";
 
 export default function Header() {
 
+    const navigate = useNavigate();
+
+    const redirectToHome = () => {
+        navigate("/")
+    }
+
     return <div className="header flex-space-between">
 
-        <div className="header-logo flex-align-center">
+        <div className="header-logo flex-align-center"  onClick={redirectToHome}>
 
             <div className="header-logo-img">
                 <img src="https://res.cloudinary.com/nakulsharma15/image/upload/v1667582669/Assets/Screenshot__350_-removebg-preview-removebg-preview_1_1_dxnrge.png" alt="th-logo" />
