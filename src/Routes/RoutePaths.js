4b0fbaa6b1c6d00c0a2a400d32e4db11ea882ab3
nakulsharma15 <nakuls309@gmail.com>
2022-11-14 22:58:44 +0530

    chore: added 404 page

--- src/Routes/RoutePaths.js
+++ src/Routes/RoutePaths.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import PomodoroPage from "../Pages/PomodoroPage";
+import NoMatch from "../Pages/NoMatch";
 
 export default function RoutePaths () {
 
@@ -8,6 +9,7 @@ export default function RoutePaths () {
         <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pomodoro" element={<PomodoroPage />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     )
 }
