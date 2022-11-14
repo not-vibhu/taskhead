aed596b8c6bd3f0a762110a4bf2b6d6d49048f4d
nakulsharma15 <nakuls309@gmail.com>
2022-11-14 14:56:45 +0530

    feat: created pomodoro timer

--- src/Components/PomodoroTimer.jsx
+++ src/Components/PomodoroTimer.jsx
@@ -1,18 +1,36 @@
+import "./Styles/PomodoroTimer.css";
 import { useState, useRef, useEffect } from "react";
 
 export default function PomodoroTimer() {
 
-  let focusDuration = 1;
+  let focusDuration = "1";
+  let breakDuration = "0.5";
+
+  const focusMinutes = Number(focusDuration);
+  const breakMinutes = Number(breakDuration);
 
   const [seconds, setSeconds] = useState(0);
+  const [pomodoroMode, setPomodoroMode] = useState("focus");
   const secondsRef = useRef(seconds);
   const intervalRef = useRef(null);
 
 
   let minutesLeft = Math.floor(seconds / 60);
   let secondsLeft = seconds % 60;
+
   if (minutesLeft < 10) minutesLeft = `0${minutesLeft}`;
   if (secondsLeft < 10) secondsLeft = `0${secondsLeft}`;
+
+  const switchPomodoroSession = (sessionName) => {
+
+    setPomodoroMode(sessionName);
+
+    let newTimeInSeconds = sessionName === "focus" ? focusMinutes * 60 : breakMinutes * 60;
+
+    setSeconds(newTimeInSeconds);
+    secondsRef.current = newTimeInSeconds;
+
+  }
 
   const handleSecondsUpdate = () => {
     secondsRef.current--;
@@ -27,7 +45,7 @@ export default function PomodoroTimer() {
     clearInterval(intervalRef.current);
 
     intervalRef.current = setInterval(() => {
-      if (secondsRef.current === 0) console.log("Time OVer");
+      if (secondsRef.current === 0) return clearInterval(intervalRef.current);
       handleSecondsUpdate();
     }, 1000);
   };
@@ -38,7 +56,12 @@ export default function PomodoroTimer() {
   }, []);
 
   return (
-    <div>PomodoroTimer
+    <div className="pomodoro-timer-div">
+      <h2>PomodoroTimer</h2>
+      <h3>{pomodoroMode}</h3>
+      <button onClick={() => switchPomodoroSession("focus")}>Focus Mode</button>
+
+      <button onClick={() => switchPomodoroSession("break")}>Break Mode</button>
       <h2>{minutesLeft} : {secondsLeft}</h2>
 
       <button onClick={handleStartInterval}>Start</button>
